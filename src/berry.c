f7b1b789c93309905625c4b4f2fc7cb7936b0eb5
poketony <57011422+poketony@users.noreply.github.com>
2023-12-25 00:19:25 +0900

    기초 작업 시작
    나무열매, string, 포켓몬명, 트레이너 클래스명 작업

--- src/berry.c
+++ src/berry.c
@@ -15,136 +15,136 @@
 #include "constants/event_object_movement.h"
 #include "constants/items.h"
 
-#define NAME_CHERI_BERRY   _("CHERI")
-#define NAME_CHESTO_BERRY  _("CHESTO")
-#define NAME_PECHA_BERRY   _("PECHA")
-#define NAME_RAWST_BERRY   _("RAWST")
-#define NAME_ASPEAR_BERRY  _("ASPEAR")
-#define NAME_LEPPA_BERRY   _("LEPPA")
-#define NAME_ORAN_BERRY    _("ORAN")
-#define NAME_PERSIM_BERRY  _("PERSIM")
-#define NAME_LUM_BERRY     _("LUM")
-#define NAME_SITRUS_BERRY  _("SITRUS")
-#define NAME_FIGY_BERRY    _("FIGY")
-#define NAME_WIKI_BERRY    _("WIKI")
-#define NAME_MAGO_BERRY    _("MAGO")
-#define NAME_AGUAV_BERRY   _("AGUAV")
-#define NAME_IAPAPA_BERRY  _("IAPAPA")
-#define NAME_RAZZ_BERRY    _("RAZZ")
-#define NAME_BLUK_BERRY    _("BLUK")
-#define NAME_NANAB_BERRY   _("NANAB")
-#define NAME_WEPEAR_BERRY  _("WEPEAR")
-#define NAME_PINAP_BERRY   _("PINAP")
-#define NAME_POMEG_BERRY   _("POMEG")
-#define NAME_KELPSY_BERRY  _("KELPSY")
-#define NAME_QUALOT_BERRY  _("QUALOT")
-#define NAME_HONDEW_BERRY  _("HONDEW")
-#define NAME_GREPA_BERRY   _("GREPA")
-#define NAME_TAMATO_BERRY  _("TAMATO")
-#define NAME_CORNN_BERRY   _("CORNN")
-#define NAME_MAGOST_BERRY  _("MAGOST")
-#define NAME_RABUTA_BERRY  _("RABUTA")
-#define NAME_NOMEL_BERRY   _("NOMEL")
-#define NAME_SPELON_BERRY  _("SPELON")
-#define NAME_PAMTRE_BERRY  _("PAMTRE")
-#define NAME_WATMEL_BERRY  _("WATMEL")
-#define NAME_DURIN_BERRY   _("DURIN")
-#define NAME_BELUE_BERRY   _("BELUE")
-#define NAME_LIECHI_BERRY  _("LIECHI")
-#define NAME_GANLON_BERRY  _("GANLON")
-#define NAME_SALAC_BERRY   _("SALAC")
-#define NAME_PETAYA_BERRY  _("PETAYA")
-#define NAME_APICOT_BERRY  _("APICOT")
-#define NAME_LANSAT_BERRY  _("LANSAT")
-#define NAME_STARF_BERRY   _("STARF")
-#define NAME_ENIGMA_BERRY  _("ENIGMA")
+#define NAME_CHERI_BERRY   _("버치")
+#define NAME_CHESTO_BERRY  _("유루")
+#define NAME_PECHA_BERRY   _("복슝")
+#define NAME_RAWST_BERRY   _("복분")
+#define NAME_ASPEAR_BERRY  _("배리")
+#define NAME_LEPPA_BERRY   _("과사")
+#define NAME_ORAN_BERRY    _("오랭")
+#define NAME_PERSIM_BERRY  _("시몬")
+#define NAME_LUM_BERRY     _("리샘")
+#define NAME_SITRUS_BERRY  _("자뭉")
+#define NAME_FIGY_BERRY    _("무화")
+#define NAME_WIKI_BERRY    _("위키")
+#define NAME_MAGO_BERRY    _("마고")
+#define NAME_AGUAV_BERRY   _("아바")
+#define NAME_IAPAPA_BERRY  _("파야")
+#define NAME_RAZZ_BERRY    _("라즈")
+#define NAME_BLUK_BERRY    _("블리")
+#define NAME_NANAB_BERRY   _("나나")
+#define NAME_WEPEAR_BERRY  _("서배")
+#define NAME_PINAP_BERRY   _("파인")
+#define NAME_POMEG_BERRY   _("유석")
+#define NAME_KELPSY_BERRY  _("시마")
+#define NAME_QUALOT_BERRY  _("파비")
+#define NAME_HONDEW_BERRY  _("로매")
+#define NAME_GREPA_BERRY   _("또뽀")
+#define NAME_TAMATO_BERRY  _("토망")
+#define NAME_CORNN_BERRY   _("수숙")
+#define NAME_MAGOST_BERRY  _("고스티")
+#define NAME_RABUTA_BERRY  _("라부탐")
+#define NAME_NOMEL_BERRY   _("노멜")
+#define NAME_SPELON_BERRY  _("메호키")
+#define NAME_PAMTRE_BERRY  _("자야")
+#define NAME_WATMEL_BERRY  _("슈박")
+#define NAME_DURIN_BERRY   _("듀리")
+#define NAME_BELUE_BERRY   _("루베")
+#define NAME_LIECHI_BERRY  _("치리")
+#define NAME_GANLON_BERRY  _("용아")
+#define NAME_SALAC_BERRY   _("캄라")
+#define NAME_PETAYA_BERRY  _("야타비")
+#define NAME_APICOT_BERRY  _("규살")
+#define NAME_LANSAT_BERRY  _("랑사")
+#define NAME_STARF_BERRY   _("스타")
+#define NAME_ENIGMA_BERRY  _("의문")
 
-static const u8 sBerryDescriptionPart1_Cheri[] = _("Blooms with delicate pretty flowers.");
-static const u8 sBerryDescriptionPart2_Cheri[] = _("The bright red BERRY is very spicy.");
-static const u8 sBerryDescriptionPart1_Chesto[] = _("The BERRY's thick skin and fruit are");
-static const u8 sBerryDescriptionPart2_Chesto[] = _("very tough. It is dry-tasting all over.");
-static const u8 sBerryDescriptionPart1_Pecha[] = _("Very sweet and delicious.");
-static const u8 sBerryDescriptionPart2_Pecha[] = _("Also very tender - handle with care.");
-static const u8 sBerryDescriptionPart1_Rawst[] = _("If the leaves grow long and curly,");
-static const u8 sBerryDescriptionPart2_Rawst[] = _("the BERRY seems to grow very bitter.");
-static const u8 sBerryDescriptionPart1_Aspear[] = _("The hard BERRY is dense with a rich");
-static const u8 sBerryDescriptionPart2_Aspear[] = _("juice. It is quite sour.");
-static const u8 sBerryDescriptionPart1_Leppa[] = _("Grows slower than CHERI and others.");
-static const u8 sBerryDescriptionPart2_Leppa[] = _("The smaller the BERRY, the tastier.");
-static const u8 sBerryDescriptionPart1_Oran[] = _("A peculiar BERRY with a mix of flavors.");
-static const u8 sBerryDescriptionPart2_Oran[] = _("BERRIES grow in half a day.");
-static const u8 sBerryDescriptionPart1_Persim[] = _("Loves sunlight. The BERRY's color");
-static const u8 sBerryDescriptionPart2_Persim[] = _("grows vivid when exposed to the sun.");
-static const u8 sBerryDescriptionPart1_Lum[] = _("Slow to grow. If raised with loving");
-static const u8 sBerryDescriptionPart2_Lum[] = _("care, it may grow two BERRIES.");
-static const u8 sBerryDescriptionPart1_Sitrus[] = _("Closely related to ORAN. The large");
-static const u8 sBerryDescriptionPart2_Sitrus[] = _("BERRY has a well-rounded flavor.");
-static const u8 sBerryDescriptionPart1_Figy[] = _("The BERRY, which looks chewed up,");
-static const u8 sBerryDescriptionPart2_Figy[] = _("brims with spicy substances.");
-static const u8 sBerryDescriptionPart1_Wiki[] = _("The BERRY is said to have grown lumpy");
-static const u8 sBerryDescriptionPart2_Wiki[] = _("to help POKéMON grip it.");
-static const u8 sBerryDescriptionPart1_Mago[] = _("The BERRY turns curvy as it grows.");
-static const u8 sBerryDescriptionPart2_Mago[] = _("The curvier, the sweeter and tastier.");
-static const u8 sBerryDescriptionPart1_Aguav[] = _("The flower is dainty. It is rare in its");
-static const u8 sBerryDescriptionPart2_Aguav[] = _("ability to grow without light.");
-static const u8 sBerryDescriptionPart1_Iapapa[] = _("The BERRY is very big and sour.");
-static const u8 sBerryDescriptionPart2_Iapapa[] = _("It takes at least a day to grow.");
-static const u8 sBerryDescriptionPart1_Razz[] = _("The red BERRY tastes slightly spicy.");
-static const u8 sBerryDescriptionPart2_Razz[] = _("It grows quickly in just four hours.");
-static const u8 sBerryDescriptionPart1_Bluk[] = _("The BERRY is blue on the outside, but");
-static const u8 sBerryDescriptionPart2_Bluk[] = _("it blackens the mouth when eaten.");
-static const u8 sBerryDescriptionPart1_Nanab[] = _("This BERRY was the seventh");
-static const u8 sBerryDescriptionPart2_Nanab[] = _("discovered in the world. It is sweet.");
-static const u8 sBerryDescriptionPart1_Wepear[] = _("The flower is small and white. It has a");
-static const u8 sBerryDescriptionPart2_Wepear[] = _("delicate balance of bitter and sour.");
-static const u8 sBerryDescriptionPart1_Pinap[] = _("Weak against wind and cold.");
-static const u8 sBerryDescriptionPart2_Pinap[] = _("The fruit is spicy and the skin, sour.");
-static const u8 sBerryDescriptionPart1_Pomeg[] = _("However much it is watered,");
-static const u8 sBerryDescriptionPart2_Pomeg[] = _("it only grows up to six BERRIES.");
-static const u8 sBerryDescriptionPart1_Kelpsy[] = _("A rare variety shaped like a root.");
-static const u8 sBerryDescriptionPart2_Kelpsy[] = _("Grows a very large flower.");
-static const u8 sBerryDescriptionPart1_Qualot[] = _("Loves water. Grows strong even in");
-static const u8 sBerryDescriptionPart2_Qualot[] = _("locations with constant rainfall.");
-static const u8 sBerryDescriptionPart1_Hondew[] = _("A BERRY that is very valuable and");
-static const u8 sBerryDescriptionPart2_Hondew[] = _("rarely seen. It is very delicious.");
-static const u8 sBerryDescriptionPart1_Grepa[] = _("Despite its tenderness and round");
-static const u8 sBerryDescriptionPart2_Grepa[] = _("shape, the BERRY is unimaginably sour.");
-static const u8 sBerryDescriptionPart1_Tamato[] = _("The BERRY is lip-bendingly spicy.");
-static const u8 sBerryDescriptionPart2_Tamato[] = _("It takes time to grow.");
-static const u8 sBerryDescriptionPart1_Cornn[] = _("A BERRY from an ancient era. May not");
-static const u8 sBerryDescriptionPart2_Cornn[] = _("grow unless planted in quantity.");
-static const u8 sBerryDescriptionPart1_Magost[] = _("A BERRY that is widely said to have");
-static const u8 sBerryDescriptionPart2_Magost[] = _("a finely balanced flavor.");
-static const u8 sBerryDescriptionPart1_Rabuta[] = _("A rare variety that is overgrown with");
-static const u8 sBerryDescriptionPart2_Rabuta[] = _("hair. It is quite bitter.");
-static const u8 sBerryDescriptionPart1_Nomel[] = _("Quite sour. Just one bite makes it");
-static const u8 sBerryDescriptionPart2_Nomel[] = _("impossible to taste for three days.");
-static const u8 sBerryDescriptionPart1_Spelon[] = _("The vividly red BERRY is very spicy.");
-static const u8 sBerryDescriptionPart2_Spelon[] = _("Its warts secrete a spicy substance.");
-static const u8 sBerryDescriptionPart1_Pamtre[] = _("Drifts on the sea from somewhere.");
-static const u8 sBerryDescriptionPart2_Pamtre[] = _("It is thought to grow elsewhere.");
-static const u8 sBerryDescriptionPart1_Watmel[] = _("A huge BERRY, with some over 20");
-static const u8 sBerryDescriptionPart2_Watmel[] = _("inches discovered. Exceedingly sweet.");
-static const u8 sBerryDescriptionPart1_Durin[] = _("Bitter to even look at. It is so");
-static const u8 sBerryDescriptionPart2_Durin[] = _("bitter, no one has ever eaten it as is.");
-static const u8 sBerryDescriptionPart1_Belue[] = _("It is glossy and looks delicious, but");
-static const u8 sBerryDescriptionPart2_Belue[] = _("it is awfully sour. Takes time to grow.");
-static const u8 sBerryDescriptionPart1_Liechi[] = _("A mysterious BERRY. It is rumored to");
-static const u8 sBerryDescriptionPart2_Liechi[] = _("contain the power of the sea.");
-static const u8 sBerryDescriptionPart1_Ganlon[] = _("A mysterious BERRY. It is rumored to");
-static const u8 sBerryDescriptionPart2_Ganlon[] = _("contain the power of the land.");
-static const u8 sBerryDescriptionPart1_Salac[] = _("A mysterious BERRY. It is rumored to");
-static const u8 sBerryDescriptionPart2_Salac[] = _("contain the power of the sky.");
-static const u8 sBerryDescriptionPart1_Petaya[] = _("A mysterious BERRY. It is rumored to");
-static const u8 sBerryDescriptionPart2_Petaya[] = _("contain the power of all living things.");
-static const u8 sBerryDescriptionPart1_Apicot[] = _("A very mystifying BERRY. No telling");
-static const u8 sBerryDescriptionPart2_Apicot[] = _("what may happen or how it can be used.");
-static const u8 sBerryDescriptionPart1_Lansat[] = _("Said to be a legendary BERRY.");
-static const u8 sBerryDescriptionPart2_Lansat[] = _("Holding it supposedly brings joy.");
-static const u8 sBerryDescriptionPart1_Starf[] = _("So strong, it was abandoned at the");
-static const u8 sBerryDescriptionPart2_Starf[] = _("world's edge. Considered a mirage.");
-static const u8 sBerryDescriptionPart1_Enigma[] = _("A completely enigmatic BERRY.");
-static const u8 sBerryDescriptionPart2_Enigma[] = _("Appears to have the power of stars.");
+static const u8 sBerryDescriptionPart1_Cheri[] = _("매우 귀여운 꽃이 핀다.");
+static const u8 sBerryDescriptionPart2_Cheri[] = _("새빨간 열매는 아주 맵다.");
+static const u8 sBerryDescriptionPart1_Chesto[] = _("겉 부분과 속 모두");
+static const u8 sBerryDescriptionPart2_Chesto[] = _("딱딱하다. 어느 부분을 먹어도 떫다.");
+static const u8 sBerryDescriptionPart1_Pecha[] = _("매우 달고 맛있다.");
+static const u8 sBerryDescriptionPart2_Pecha[] = _("부드러워서 운반할 때는 주의.");
+static const u8 sBerryDescriptionPart1_Rawst[] = _("잎사귀의 부분이 길고 둥글게");
+static const u8 sBerryDescriptionPart2_Rawst[] = _("말려 있으면 아주 쓴 듯하다.");
+static const u8 sBerryDescriptionPart1_Aspear[] = _("딱딱한 열매 속에는 수분이 매우");
+static const u8 sBerryDescriptionPart2_Aspear[] = _("많이 들어 있다. 꽤 시다.");
+static const u8 sBerryDescriptionPart1_Leppa[] = _("버치 등과 비교해 느리게 자란다.");
+static const u8 sBerryDescriptionPart2_Leppa[] = _("열매가 작은 것일수록 맛있다.");
+static const u8 sBerryDescriptionPart1_Oran[] = _("여러 가지 맛이 섞인 신기한 열매.");
+static const u8 sBerryDescriptionPart2_Oran[] = _("열매를 키우는 데 반나절이 걸린다.");
+static const u8 sBerryDescriptionPart1_Persim[] = _("태양을 좋아한다. 햇빛을 받으면");
+static const u8 sBerryDescriptionPart2_Persim[] = _("점점 색이 선명해진다.");
+static const u8 sBerryDescriptionPart1_Lum[] = _("자라는 데 시간이 걸리지만 소중하게");
+static const u8 sBerryDescriptionPart2_Lum[] = _("키우면 열매가 2개 자라기도 한다.");
+static const u8 sBerryDescriptionPart1_Sitrus[] = _("오랭과는 형제 같은 존재.");
+static const u8 sBerryDescriptionPart2_Sitrus[] = _("크고 맛도 고르다.");
+static const u8 sBerryDescriptionPart1_Figy[] = _("먹힌 듯한 모양 속에는");
+static const u8 sBerryDescriptionPart2_Figy[] = _("매운 성분이 가득 차 있다.");
+static const u8 sBerryDescriptionPart1_Wiki[] = _("주위에 붙어 있는 돌기는 포켓몬이");
+static const u8 sBerryDescriptionPart2_Wiki[] = _("지니기 쉽게 하기 위해서라고 한다.");
+static const u8 sBerryDescriptionPart1_Mago[] = _("크게 자랄수록 열매가 구부러진다.");
+static const u8 sBerryDescriptionPart2_Mago[] = _("구부러질수록 달고 맛있다.");
+static const u8 sBerryDescriptionPart1_Aguav[] = _("작은 크기의 꽃으로 그것도 빛을");
+static const u8 sBerryDescriptionPart2_Aguav[] = _("필요로 하지 않는 희귀한 나무열매다.");
+static const u8 sBerryDescriptionPart1_Iapapa[] = _("매우 크고 신 나무열매다.");
+static const u8 sBerryDescriptionPart2_Iapapa[] = _("키우는 데는 적어도 1일이 걸린다.");
+static const u8 sBerryDescriptionPart1_Razz[] = _("빨간 열매는 먹으면 약간 맵고");
+static const u8 sBerryDescriptionPart2_Razz[] = _("빠르게 자라서 4시간이면 열매가 열린다.");
+static const u8 sBerryDescriptionPart1_Bluk[] = _("겉 부분은 파란색이지만 먹으면");
+static const u8 sBerryDescriptionPart2_Bluk[] = _("입속이 새까맣게 된다.");
+static const u8 sBerryDescriptionPart1_Nanab[] = _("세상에서 7번째로 발견된 것이");
+static const u8 sBerryDescriptionPart2_Nanab[] = _("이름의 유래라는 설이 유력하다. 달다.");
+static const u8 sBerryDescriptionPart1_Wepear[] = _("작고 하얀 꽃이 핀다. 쓴맛과");
+static const u8 sBerryDescriptionPart2_Wepear[] = _("신맛이 섞여 있는 미묘한 맛이다.");
+static const u8 sBerryDescriptionPart1_Pinap[] = _("바람에 약하고 추위에도 약하다.");
+static const u8 sBerryDescriptionPart2_Pinap[] = _("열매의 속이 맵고 겉 부분은 시다.");
+static const u8 sBerryDescriptionPart1_Pomeg[] = _("아무리 비가 내려도 영양이");
+static const u8 sBerryDescriptionPart2_Pomeg[] = _("있어도 6개의 열매밖에 자라지 않는다.");
+static const u8 sBerryDescriptionPart1_Kelpsy[] = _("뿌리 같은 모양의 희귀한");
+static const u8 sBerryDescriptionPart2_Kelpsy[] = _("종류이다. 매우 큰 꽃이 핀다.");
+static const u8 sBerryDescriptionPart1_Qualot[] = _("물을 좋아한다. 계속 비가 내리는");
+static const u8 sBerryDescriptionPart2_Qualot[] = _("장소라도 쑥쑥 잘 자란다.");
+static const u8 sBerryDescriptionPart1_Hondew[] = _("매우 비싸서 좀처럼 눈으로");
+static const u8 sBerryDescriptionPart2_Hondew[] = _("보기 힘든 나무열매. 아주 맛있다.");
+static const u8 sBerryDescriptionPart1_Grepa[] = _("부드러움과 둥근 모양으로는");
+static const u8 sBerryDescriptionPart2_Grepa[] = _("상상할 수 없을 정도로 시다.");
+static const u8 sBerryDescriptionPart1_Tamato[] = _("입술이 새빨개질 정도로 맵다.");
+static const u8 sBerryDescriptionPart2_Tamato[] = _("키우는 데에도 시간이 걸린다.");
+static const u8 sBerryDescriptionPart1_Cornn[] = _("옛날부터 자라왔다. 많이 심지");
+static const u8 sBerryDescriptionPart2_Cornn[] = _("않으면 열매가 열리지 않을 때도 있다.");
+static const u8 sBerryDescriptionPart1_Magost[] = _("매우 맛의 균형이 좋다고");
+static const u8 sBerryDescriptionPart2_Magost[] = _("평판이 자자한 나무열매다.");
+static const u8 sBerryDescriptionPart1_Rabuta[] = _("많은 털이 자라있는 희귀한");
+static const u8 sBerryDescriptionPart2_Rabuta[] = _("종류이다. 꽤 쓰다.");
+static const u8 sBerryDescriptionPart1_Nomel[] = _("꽤 시다. 한입 먹으면");
+static const u8 sBerryDescriptionPart2_Nomel[] = _("3일간 다른 맛이 나지 않게 된다.");
+static const u8 sBerryDescriptionPart1_Spelon[] = _("새빨개진 열매는 말도 못하게 맵다.");
+static const u8 sBerryDescriptionPart2_Spelon[] = _("돌기로부터도 매운 성분이 나오고 있다.");
+static const u8 sBerryDescriptionPart1_Pamtre[] = _("어딘가에서 난데없이 떠내려온다.");
+static const u8 sBerryDescriptionPart2_Pamtre[] = _("세상 어딘가에서 키워지고 있는 듯하다.");
+static const u8 sBerryDescriptionPart1_Watmel[] = _("매우 커서 50cm를 넘는 것도");
+static const u8 sBerryDescriptionPart2_Watmel[] = _("발견되었다. 굉장히 달다");
+static const u8 sBerryDescriptionPart1_Durin[] = _("보기만 해도 쓰다. 너무나도 써서");
+static const u8 sBerryDescriptionPart2_Durin[] = _("그대로 먹은 사람은 아무도 없다.");
+static const u8 sBerryDescriptionPart1_Belue[] = _("매끈해서 맛있어 보이지만 말도 못하게");
+static const u8 sBerryDescriptionPart2_Belue[] = _("시다. 키우는 데에도 시간이 걸린다.");
+static const u8 sBerryDescriptionPart1_Liechi[] = _("수수께끼의 씨앗. 치리열매에는");
+static const u8 sBerryDescriptionPart2_Liechi[] = _("바다의 힘이 깃들어 있다는 소문이 있다.");
+static const u8 sBerryDescriptionPart1_Ganlon[] = _("수수께끼의 씨앗. 용아열매에는");
+static const u8 sBerryDescriptionPart2_Ganlon[] = _("육지의 힘이 깃들어 있다는 소문이 있다.");
+static const u8 sBerryDescriptionPart1_Salac[] = _("수수께끼의 씨앗. 캄라열매에는");
+static const u8 sBerryDescriptionPart2_Salac[] = _("하늘의 힘이 깃들어 있다는 소문이 있다.");
+static const u8 sBerryDescriptionPart1_Petaya[] = _("수수께끼의 씨앗. 야타비열매에는 모든");
+static const u8 sBerryDescriptionPart2_Petaya[] = _("생명체의 힘이 깃들어 있다고 전해진다.");
+static const u8 sBerryDescriptionPart1_Apicot[] = _("이상하고 이상한 나무열매다. 무엇이");
+static const u8 sBerryDescriptionPart2_Apicot[] = _("일어날지 무엇이 생길지 알 수 없다.");
+static const u8 sBerryDescriptionPart1_Lansat[] = _("전설의 열매라고 전해진다. 이 열매를");
+static const u8 sBerryDescriptionPart2_Lansat[] = _("가지고 있으면 매우 행복해지는 듯하다.");
+static const u8 sBerryDescriptionPart1_Starf[] = _("너무나도 힘이 강하기 때문에");
+static const u8 sBerryDescriptionPart2_Starf[] = _("세상의 끝에 버려진 나무열매다.");
+static const u8 sBerryDescriptionPart1_Enigma[] = _("정체불명의 나무열매다.");
+static const u8 sBerryDescriptionPart2_Enigma[] = _("별의 힘을 지니고 있는 듯하다.");
 
 const struct Berry gBerries[] =
 {
@@ -940,8 +940,8 @@ extern const u8 gSpriteImage_UnusedCherry[];
 extern const u16 gSpritePalette_UnusedCherry[];
 extern u8 gUnknown_Debug_839B6CE[];
 
-static const u8 gUnknown_Debug_083F7F84[] = DTR("そとから　きた　きのみ", "An exterior BERRY");
-static const u8 gUnknown_Debug_083F7F90[] = DTR("ただいま　かいはつちゅう", "Currently under development");
+static const u8 gUnknown_Debug_083F7F84[] = DTR("そとから　きた　きのみ", "외부에서 온 나무열매");
+static const u8 gUnknown_Debug_083F7F90[] = DTR("ただいま　かいはつちゅう", "현재 개발 중");
 
 void debug_sub_80C2C18(const u8 *name, u8 holdEffect, u8 holdEffectParam)
 {
